15cd05dc60f48f1e7e07255e8001391d45008ce1
sfaat <sfaathusainpintu@gmail.com>
2025-04-18 00:03:40 +0530

    add a role model.

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(TimeStampedModel):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
